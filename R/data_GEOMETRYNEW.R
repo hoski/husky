6c5f76af6cb03ff043c1477f189cfb5db06be5c5
Einar Hjorleifsson <einarhj@hafstokkur.hafro.is>
2016-12-07 16:13:51 +0000

    work on strata mess

--- R/data_GEOMETRYNEW.R
+++ R/data_GEOMETRYNEW.R
@@ -13,3 +13,20 @@ NULL
 #' @docType data
 #' @author Hoskuldur Bjornsson
 NULL
+
+
+#' @name stratas_df
+#' @title XXX
+#' @description XXX
+#'
+#' x <- attributes(STRATAS)
+#' stratas_df <- data.frame(strata = as.integer(x$names),
+#' area = x$area,
+#' rall.area = x$rall.area,
+#' lon = x$pos[,2],
+#' lat = x$pos[,1])
+#' use_data(stratas_df)
+#'
+#' @docType data
+#' @author Einar Hjorleifsson
+NULL
---
+++ R/data_HAUSTRALLNewStrata_Stratifiering.R
@@ -0,0 +1,24 @@
+#' @name NEWSTRATAS
+#' @title XXX
+#' @description XXX
+#' location: /net/hafkaldi/export/u2/reikn/Splus5/HAUSTRALLNewStrata/Stratifiering/.RData
+#' NEWSTRATAS <- STRATA
+#' use_data(NEWSTRATAS)
+#' @docType data
+#' @author Hoskuldur Bjornsson
+NULL
+
+#' @name newstratas_df
+#' @title XXX
+#' @description XXX
+#' x <- attributes(NEWSTRATAS)
+#' newstratas_df <- data_frame(strata = 1:length(x$names),
+#'                 names = x$names,
+#'                 area = x$area,
+#'                 rall.area = x$rall.area,
+#'                 lon = x$pos[,2],
+#'                 lat = x$pos[,1])
+#' use_data(newstratas)
+#' @docType data
+#' @author Einar Hjorleifsson
+NULL
