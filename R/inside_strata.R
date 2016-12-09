d4e9cc5508a63a9dbe8bee303b9b6f7fe938348e
Einar Hjorleifsson <einarhj@hafbjarmi.hafro.is>
2016-12-09 10:53:37 +0000

    added inside.strata

---
+++ R/inside_strata.R
@@ -0,0 +1,26 @@
+#' inside.strata
+#'
+#' location: /net/hafkaldi/export/u2/reikn/Splus5/SMB/GEOMETRY.NEW/.RData
+#'
+#' @param stodvar XXX
+#' @param stratalist XXX
+#'
+#' @export
+#'
+inside.strata <- function (stodvar, stratalist = ralllist)
+{
+  stodvar$newstrata <- rep(0, nrow(stodvar))
+  for (i in stratalist) {
+    j <- geo::geoinside(stodvar, reg = STRATAS[[i]], robust = T,
+                        option = 0)
+    if (length(j) > 0)
+      stodvar[j, "newstrata"] <- i
+  }
+  i <- stodvar$newstrata == 0
+  if (any(i)) {
+    i1 <- 1:length(i)
+    i1 <- i1[i]
+    cat("Strata fannst ekki fyrir stodvar", paste(i1, collapse = ","))
+  }
+  return(stodvar)
+}
